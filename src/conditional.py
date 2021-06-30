bfc1ce877ae1e712e100af425136be40bbcc46e9
Andrew Sipes <andrew.n.sipes@gmail.com>
2021-06-30 19:41:47 -0400

    Add conditionals example.

---
+++ src/conditional.py
@@ -0,0 +1,10 @@
+def main():
+  cold = False
+  open = True
+
+  if open: print('Throw in the trash')
+  elif not cold: print('Throw in the fridge')
+  else: print('Throw it down the hatch')
+
+
+if __name__ == "__main__": main()
