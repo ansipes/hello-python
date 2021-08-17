6f1c7ef37b6949cc6f37543c711336050b7dd36c
Andrew Sipes <andrew.n.sipes@gmail.com>
2021-08-17 19:29:12 -0400

    Refactor w/ Ryan.

--- src/static/exercises/e9_guessing_game.py
+++ src/static/exercises/e9_guessing_game.py
@@ -7,24 +7,24 @@ g = []
 x = random.randint(floor, ceiling)
 
 
-def warnIfOutOfRange(n):
-    if (n > ceiling):
-        print(f"Your guess is higher than the ceiling, which is {ceiling}")
-        return
-
+def warnIfOutOfRange(n, floor, ceiling):
     if (n < floor):
         print(f"Your guess is lower than the floor, which is {floor}")
         return
 
+    if (n > ceiling):
+        print(f"Your guess is higher than the ceiling, which is {ceiling}")
+        return
 
-def warnIfDuplicateGuess(n):
+
+def warnIfDuplicateGuess(n, g):
     if (n in g):
         print("You have already guessed this number.")
 
     g.append(n)
 
 
-def giveHint(n):
+def giveHint(n, x):
     if (n > x):
         print("Your guess is too high.")
         return
@@ -34,14 +34,16 @@ def giveHint(n):
         return
 
 
-def summary():
+def summary(x, g):
+    print("")
     print(f"The random number was: {x}")
-    print(f"You guess {len(g)} times.")
+    print(f"You guessed {len(g)} time{('', 's')[len(g) > 1]}.")
     print(f"Here are you guesses:")
     print(g)
 
 
-print("I've picked a random number between 1 and 9. Can you guess it?")
+print(
+    f"I've picked a random number between {floor} and {ceiling}. Can you guess it?")
 while True:
     i = input("Guess a number: ")
     if (i == "exit"):
@@ -50,14 +52,14 @@ while True:
 
     n = int(i)
 
-    warnIfOutOfRange(n)
+    warnIfOutOfRange(n, floor, ceiling)
 
-    warnIfDuplicateGuess(n)
+    warnIfDuplicateGuess(n, g)
 
     if (n == x):
         print("You win!")
         break
 
-    giveHint(n)
+    giveHint(n, x)
 
-summary()
+summary(x, g)
