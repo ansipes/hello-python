84501022f38528ff57318de3465cfdaa8d1d8a40
Andrew Sipes <andrew.n.sipes@gmail.com>
2021-09-08 14:24:30 -0400

    Use main pattern.

--- src/static/exercises/e11_check_primality.py
+++ src/static/exercises/e11_check_primality.py
@@ -1,12 +1,15 @@
+def main():
+  n = int(input("Enter a number: "))
+
+  if isPrime(n):
+    print("This number is prime.")
+  else:
+    print("This number is not prime.")
+
 def isPrime(n):
   divisors = [x for x in range(1, n // 2 + 1) if n % x == 0]
   divisors.append(n)
 
   return len(divisors) == 2
 
-n = int(input("Enter a number: "))
-
-if isPrime(n):
-  print("This number is prime.")
-else:
-  print("This number is not prime.")
+if __name__ == "__main__": main()
